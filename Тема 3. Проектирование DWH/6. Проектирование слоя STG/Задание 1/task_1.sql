9cff9642026ea9e263e64fc3ddcf0d0f5129c6bf
pumpkinorange <165155445+pumpkinorange@users.noreply.github.com>
2026-04-14 17:42:56 +0300

    Решение Задание 1 / Тема 3. Проектирование DWH / 6. Проектирование слоя STG

---
+++ Тема 3. Проектирование DWH/6. Проектирование слоя STG/Задание 1/task_1.sql
@@ -0,0 +1,24 @@
+CREATE TABLE stg.bonussystem_users (
+	id int4 GENERATED ALWAYS AS IDENTITY( INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START 1 CACHE 1 NO CYCLE) NOT NULL,
+	order_user_id text NOT NULL,
+	CONSTRAINT users_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE stg.bonussystem_ranks (
+	id int4 NOT NULL,
+	"name" varchar(2048) NOT NULL,
+	bonus_percent numeric(19, 5) DEFAULT 0 NOT NULL,
+	min_payment_threshold numeric(19, 5) DEFAULT 0 NOT NULL,
+	CONSTRAINT ranks_bonus_percent_check CHECK ((bonus_percent >= (0)::numeric)),
+	CONSTRAINT ranks_bonus_percent_check1 CHECK ((bonus_percent >= (0)::numeric)),
+	CONSTRAINT ranks_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE stg.bonussystem_events (
+	id int4 GENERATED ALWAYS AS IDENTITY( INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START 1 CACHE 1 NO CYCLE) NOT NULL,
+	event_ts timestamp NOT NULL,
+	event_type varchar NOT NULL,
+	event_value text NOT NULL,
+	CONSTRAINT bonussystem_events_pkey PRIMARY KEY (id)
+);
+CREATE INDEX idx_bonussystem_events__event_ts ON stg.bonussystem_events USING btree (event_ts);
