5eb34254092513855c5929ec0b81fae2faef5d15
pumpkinorange <165155445+pumpkinorange@users.noreply.github.com>
2026-04-14 16:11:08 +0300

    Решение задание 3 / Тема 3 / 5 Проектирование слоя CDM

---
+++ Тема 3. Проектирование DWH/5. Проектирование слоя CDM/Задание 3/realization.sql
@@ -0,0 +1,3 @@
+alter table cdm.dm_settlement_report
+    add constraint dm_settlement_report_settlement_date_check
+        check (settlement_date >= '2022-01-01' and settlement_date < '2500-01-01')
