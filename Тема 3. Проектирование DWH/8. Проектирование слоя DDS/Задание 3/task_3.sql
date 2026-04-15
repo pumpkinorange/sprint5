b5d36bda0e5bbb01c3093c5a3a90fa70b0a27bce
pumpkinorange <165155445+pumpkinorange@users.noreply.github.com>
2026-04-15 15:40:12 +0300

    s5 / topic 3 / 8 dds / task 3

---
+++ Тема 3. Проектирование DWH/8. Проектирование слоя DDS/Задание 3/task_3.sql
@@ -0,0 +1,8 @@
+create table if not exists dds.dm_restaurants
+(
+    id int4 GENERATED ALWAYS AS IDENTITY( INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START 1 CACHE 1 NO CYCLE) NOT NULL,
+    restaurant_id varchar not null,
+    restaurant_name varchar not null,
+    active_from timestamp not null,
+    active_to timestamp not null
+);
