9aefa5aa9468e1d41dc2a190d19b598cf1be48f9
pumpkinorange <165155445+pumpkinorange@users.noreply.github.com>
2026-04-15 15:47:46 +0300

    s5 / topic 3 / 8 dds / task 4

---
+++ Тема 3. Проектирование DWH/8. Проектирование слоя DDS/Задание 4/task_4.sql
@@ -0,0 +1,10 @@
+create table if not exists dds.dm_products
+(
+    id int4 GENERATED ALWAYS AS IDENTITY( INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START 1 CACHE 1 NO CYCLE) NOT NULL,
+    product_id varchar not null,
+    product_name varchar not null,
+    product_price numeric(14, 2) not null default 0 constraint dm_products_count_check check (product_price >= 0),
+    restaurant_id int not null,
+    active_from timestamp not null,
+    active_to timestamp not null    
+);
