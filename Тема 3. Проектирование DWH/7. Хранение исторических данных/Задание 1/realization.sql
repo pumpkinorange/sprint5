dec7a71a9a4b6e414c04412de798e5020b720dc5
pumpkinorange <165155445+pumpkinorange@users.noreply.github.com>
2026-04-15 14:44:25 +0300

    Правки

--- Тема 3. Проектирование DWH/7. Хранение исторических данных/Задание 1/realization.sql
+++ Тема 3. Проектирование DWH/7. Хранение исторических данных/Задание 1/realization.sql
@@ -1,3 +1,34 @@
+-- Исходный DDL для таблиц clients, products и sales
+-- CREATE TABLE clients
+-- (
+--     client_id INTEGER NOT NULL
+--         CONSTRAINT clients_pk PRIMARY KEY,
+--     name      TEXT    NOT NULL,
+--     login     TEXT    NOT NULL
+-- );
+
+-- CREATE TABLE products
+-- (
+--     product_id INTEGER        NOT NULL
+--         CONSTRAINT products_pk PRIMARY KEY,
+--     name       TEXT           NOT NULL,
+--     price      NUMERIC(14, 2) NOT NULL
+-- );
+
+-- CREATE TABLE sales
+-- (
+--     client_id  INTEGER        NOT NULL
+--         CONSTRAINT sales_clients_client_id_fk REFERENCES clients,
+--     product_id INTEGER        NOT NULL
+--         CONSTRAINT sales_products_product_id_fk REFERENCES products,
+--     amount     INTEGER        NOT NULL,
+--     total_sum  NUMERIC(14, 2) NOT NULL,
+--     CONSTRAINT sales_pk PRIMARY KEY (client_id, product_id)
+-- );
+
+
+
+
 update public.clients
 set login = 'arthur_dent'
 where client_id = 42;
